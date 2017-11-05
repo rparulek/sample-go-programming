815176ac2e795a502e88df69330ecf8438ac4649
Rohan Parulekar <rohan.s.parulekar@nuagenetworks.net>
2017-11-04 19:49:40 -0700

    Change in ds file and build script

--- examples/SIMPLE-WEB-APP-PYTHON/build-app.sh
+++ examples/SIMPLE-WEB-APP-PYTHON/build-app.sh
@@ -1,1 +1,2 @@
-docker build -t web-app-img:latest .
+docker build -t docker/next-bus-api:v1.0.0 .
+docker save docker/next-bus-api:v1.0.0 > next-bus-api-docker-img-1.0.0.tar
--- examples/SIMPLE-WEB-APP-PYTHON/start-app.sh
+++ examples/SIMPLE-WEB-APP-PYTHON/start-app.sh
@@ -1,8 +1,8 @@
-docker run -d -p 5001:5001 -e endpoint="agencyList" web-app-img:latest
-docker run -d -p 5002:5002 -e endpoint="routeList" web-app-img:latest
-docker run -d -p 5003:5003 -e endpoint="routeConfig" web-app-img:latest
-docker run -d -p 5004:5004 -e endpoint="predictions" web-app-img:latest
-docker run -d -p 5005:5005 -e endpoint="predictionsForMultiStops" web-app-img:latest
-docker run -d -p 5006:5006 -e endpoint="schedule" web-app-img:latest
-docker run -d -p 5007:5007 -e endpoint="vehicleLocations" web-app-img:latest
-docker run -d -p 5008:5008 -e endpoint="messages" web-app-img:latest
+docker run -d -p 5001:5001 -e endpoint="agencyList" docker/next-bus-api:v1.0.0
+docker run -d -p 5002:5002 -e endpoint="routeList" docker/next-bus-api:v1.0.0
+docker run -d -p 5003:5003 -e endpoint="routeConfig" docker/next-bus-api:v1.0.0
+docker run -d -p 5004:5004 -e endpoint="predictions" docker/next-bus-api:v1.0.0
+docker run -d -p 5005:5005 -e endpoint="predictionsForMultiStops" docker/next-bus-api:v1.0.0
+docker run -d -p 5006:5006 -e endpoint="schedule" docker/next-bus-api:v1.0.0
+docker run -d -p 5007:5007 -e endpoint="vehicleLocations" docker/next-bus-api:v1.0.0
+docker run -d -p 5008:5008 -e endpoint="messages" docker/next-bus-api:v1.0.0
